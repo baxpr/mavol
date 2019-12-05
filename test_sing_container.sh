57c0a595b8325efc1c4d4d5edb92aa555fdeb539
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-12-04 19:51:55 -0600

    Nii in sing

--- test_sing_container.sh
+++ test_sing_container.sh
@@ -7,6 +7,6 @@ singularity run \
 --bind OUTPUTS:/OUTPUTS \
 mavol-v1.0.0.simg \
 assr_label TESTPROJ-x-TESTSUBJ-x-TESTSESS-x-TESTSCAN-x-MultiAtlas \
-seg_niigz /INPUTS/orig_target_seg_ticv.nii.gz \
+seg_niigz /INPUTS/orig_target_seg.nii.gz \
 vol_txt /INPUTS/target_processed_label_volumes.txt \
 out_dir /OUTPUTS
