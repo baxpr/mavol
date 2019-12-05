700fb4cd38a93fa322fb24506dbba961e5fbdee6
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-12-05 11:27:24 -0600

    Test script

--- test_sing_container.sh
+++ test_sing_container.sh
@@ -2,11 +2,25 @@
 
 singularity run \
 --cleanenv \
---home `pwd`/INPUTS \
---bind INPUTS:/INPUTS \
---bind OUTPUTS:/OUTPUTS \
+--home `pwd`/INPUTS1 \
+--bind INPUTS1:/INPUTS1 \
+--bind OUTPUTS1:/OUTPUTS1 \
 mavol-v1.0.0.simg \
 assr_label TESTPROJ-x-TESTSUBJ-x-TESTSESS-x-TESTSCAN-x-MultiAtlas \
-seg_niigz /INPUTS/orig_target_seg.nii.gz \
-vol_txt /INPUTS/target_processed_label_volumes.txt \
-out_dir /OUTPUTS
+seg_niigz /INPUTS1/orig_target_seg.nii.gz \
+vol_txt /INPUTS1/target_processed_label_volumes.txt \
+out_dir /OUTPUTS1
+
+
+
+singularity run \
+--cleanenv \
+--home `pwd`/INPUTS2 \
+--bind INPUTS2:/INPUTS2 \
+--bind OUTPUTS2:/OUTPUTS2 \
+mavol-v1.0.0.simg \
+assr_label TESTPROJ-x-TESTSUBJ-x-TESTSESS-x-TESTSCAN-x-MultiAtlas_v2 \
+seg_niigz /INPUTS2/orig_target_seg.nii.gz \
+ticv_niigz /INPUTS2/orig_target_ticv.nii.gz \
+vol_txt /INPUTS2/target_processed_label_volumes.txt \
+out_dir /OUTPUTS2
