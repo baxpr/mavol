e3c3a69d5d945df51202d317e08786fb6209304e
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-12-05 05:01:17 -0600

    compiled

--- bin/test_compiled_matlab.sh
+++ bin/test_compiled_matlab.sh
@@ -3,7 +3,8 @@
 xvfb-run --server-num=$(($$ + 99)) \
 --server-args='-screen 0 1600x1200x24 -ac +extension GLX' \
 bash run_mavol.sh /usr/local/MATLAB/MATLAB_Runtime/v97 \
-assr_label TESTPROJ-x-TESTSUBJ-x-TESTSESS-x-TESTSCAN-x-MultiAtlas \
-seg_niigz ../INPUTS/orig_target_seg_ticv.nii.gz \
+assr_label TESTPROJ-x-TESTSUBJ-x-TESTSESS-x-TESTSCAN-x-MultiAtlas_v2 \
+seg_niigz ../INPUTS/orig_target_seg.nii.gz \
+ticv_niigz ../INPUTS/orig_target_ticv.nii.gz \
 vol_txt ../INPUTS/target_processed_label_volumes.txt \
 out_dir ../OUTPUTS
