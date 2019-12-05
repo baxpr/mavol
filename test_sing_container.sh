04a90db496cf9ab683ad04567aefe06538e9514d
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-12-04 19:22:51 -0600

    Updated test

--- test_sing_container.sh
+++ test_sing_container.sh
@@ -5,7 +5,7 @@ singularity run \
 --home `pwd`/INPUTS \
 --bind INPUTS:/INPUTS \
 --bind OUTPUTS:/OUTPUTS \
-baxpr-mavol-master-v1.0.0.simg \
+mavol-v1.0.0.simg \
 assr_label TESTPROJ-x-TESTSUBJ-x-TESTSESS-x-TESTSCAN-x-MultiAtlas \
 seg_niigz /INPUTS/orig_target_seg_ticv.nii.gz \
 vol_txt /INPUTS/target_processed_label_volumes.txt \
